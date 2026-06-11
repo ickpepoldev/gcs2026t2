5d211f5a41ca828947331036dcba47ca7314522c
Kaleb Lucas Lau <bakingwithkid@gmail.com>
2026-06-11 10:59:50 +0800

    Update deployment to use gh-pages branch

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gcs-project/',
+  base: '/gcs2026t2/',
   server: {
     port: 5173,
     proxy: {
